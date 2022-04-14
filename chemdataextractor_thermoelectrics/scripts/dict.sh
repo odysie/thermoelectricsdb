e72b99603787ecf8e9ec2d112b1a9e3bc5390fca
Odysseas Sierepeklis <35266236+odysie@users.noreply.github.com>
2022-04-14 17:08:44 +0100

    change details

--- chemdataextractor_thermoelectrics/scripts/dict.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-cde dict build data/cde-dict/jochem-cs.txt data/cde-dict/include.txt --output data/models/dict_chem_cs.pickle --cs
-cde dict build data/cde-dict/jochem.txt --output data/models/dict_chem.pickle
